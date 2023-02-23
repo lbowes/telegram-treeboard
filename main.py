6c35f31786cf4b607c02228cf95e0bab42e260fd
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2023-02-23 02:20:12 +0000

    Add treelib example (to update)

--- main.py
+++ main.py
@@ -12,9 +12,17 @@ def main() -> None:
     )
 
     from telegram_treeboard import Treeboard
+    from treelib import Node, Tree
 
-    # todo: store a tree of options here
-    tree = {}
+    # todo: update example
+    tree = Tree()
+    tree.create_node("Harry", "harry")  # root node
+    tree.create_node("Jane", "jane", parent="harry")
+    tree.create_node("Bill", "bill", parent="harry")
+    tree.create_node("Diane", "diane", parent="jane")
+    tree.create_node("Mary", "mary", parent="diane")
+    tree.create_node("Mark", "mark", parent="jane")
+
     Treeboard(application, tree)
 
     # Run the bot until the user presses Ctrl-C
